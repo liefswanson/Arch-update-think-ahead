e88ddc24480ebd720faa6cfe986ae6a6dbd3fd67
liefswanson <liefs@sfu.ca>
2014-10-16 15:24:21 -0700

    made the most recent news open in default browser

--- news.sh
+++ news.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#mainpage=curl https://archlinux.org/news/
-#mainpage=$(cat ./news.html)
 
 archnews="https://archlinux.org/news/"
 
 #grabs the relative link to the most recent news post then completes it into a complete url
-firsturl=$archnews"$(cat ./news.html | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
+#firsturl=archnews"$(curl $archnews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
+firsturl=$archnews"$(cat news.html | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
 
 echo $firsturl
 
 #grabs the date of the most recent news post
-dateofnews="$(cat ./news.html | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
+#dateofurl="$(curl $archnews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
+dateofurl="$(cat news.html | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
 
-echo $dateofnews
+echo $dateofurl
+
+xdg-open $firsturl
