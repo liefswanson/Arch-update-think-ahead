da1f13d1f8900c18916d02338be540e64050ad52
liefswanson <liefs@sfu.ca>
2014-10-16 23:23:14 -0700

    made the script useable!

--- news.sh
+++ news.sh
@@ -8,48 +8,57 @@ confDir=$HOME"/.anh/"
 tempCache=$confDir"tempCache.html"
 staleCache=$confDir"cache.html"
 
+
+if [ ! -d $confDir ]
+then
+	mkdir $confDir
+fi
+
 #curl $archNews > $tempCache
 cat $localNews > $tempCache
 
 
-#grabs the relative link to the most recent news post then completes it into a complete url
 URL=$archNews"$(cat $tempCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)"
-
-echo $URL
-
 title="$(cat $tempCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
-
-echo $title
-
-#grabs the date of the most recent news post
 date="$(cat $tempCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
 
-echo $date
+# echo $URL
+# echo $title
+# echo $date
 
-staleURL=$archNews"$(cat $staleCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)" 
-staleTitle="$(cat $staleCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
-staleDate="$(cat $staleCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
-
-echo $staleURL
-echo $staleTitle
-echo $staleDate
-
-if [ ! "$URL" == "$staleURL" -a "$title" == "$staleTitle" -a "$date" == "$staleDate" ]
+if [ -f $staleCache ]
 then
+	staleURL=$archNews"$(cat $staleCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)" 
+	staleTitle="$(cat $staleCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
+	staleDate="$(cat $staleCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
 
-	
-	
+	# echo $staleURL
+	# echo $staleTitle
+	# echo $staleDate
+fi
+
+if [ ! "$URL" == "$staleURL" -a "$title" == "$staleTitle" -a "$date" == "$staleDate" -o ! -f $staleCache ]
+then
+	echo "There is unread news on $archNews"
+	echo 
+	echo "$title"
+	echo "Posted: $date"
+	echo 
+
 	read -p "Do you wish to open the most recent news in your default browser? [y/N/a]" browse
 	case $browse in
-		[Yy]* ) xdg-open $postURL;;
+		[Yy]* ) xdg-open $URL;;
 		[Aa]* ) exit;;
 	esac
 
-	sleep 10
+	cat $tempCache > $staleCache
+	rm $tempCache
 
 	if [ -f "/usr/bin/pacman" ]
 	then
-		yaourt -Syu
+		sudo pacman -Syu
+	else
+		echo "You don't have pacman on your system. If you are on Arch, you should panic."
 	fi
 else
 	echo "No new news."
