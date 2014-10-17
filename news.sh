6a94e907de294b9862f1c3821165b196c8561495
liefswanson <liefs@sfu.ca>
2014-10-17 14:57:13 -0700

    Deleted a file added by my poor bash coding, and made the script a cache in a directory under home. The script now actually curls the arch news page instead of the local cached version which existed for testing. The only things left to do are detect pacman wrappers, and find a way to output the news to the console.

--- news.sh
+++ news.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-archNews="https://archlinux.org/news/"
+archNews="https://www.archlinux.org/news/"
 #for testing so I don't DDOS archlinux.org by accident
 localNews="news.html"
 
 confDir=$HOME"/.anh/"
 tempCache=$confDir"tempCache.html"
-staleCache=$confDir"cache.html"
-
 
 if [ ! -d $confDir ]
 then
 	mkdir $confDir
 fi
 
-#curl $archNews > $tempCache
-cat $localNews > $tempCache
+curl $archNews > $tempCache
+#cat $localNews > $tempCache
 
 
-URL=$archNews"$(cat $tempCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)"
+URL=$archNews"$(cat $tempCache | grep href=\"/news/ | head -1 | cut -d\" -f 4 | cut -c 7-)"
 title="$(cat $tempCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
 date="$(cat $tempCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
+
+#temp cache no longer needed.
 
 # echo $URL
 # echo $title
 # echo $date
 
-if [ -f $staleCache ]
+if [ -f $confDir"/url" -a -f $confDir"/title" -a -f $confDir"/date" ]
 then
-	staleURL=$archNews"$(cat $staleCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)" 
-	staleTitle="$(cat $staleCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
-	staleDate="$(cat $staleCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
+	staleURL="$(cat $confDir/url)"
+	staleTitle="$(cat $confDir/title)"
+	staleDate="$(cat $confDir/date)"
 
 	# echo $staleURL
 	# echo $staleTitle
 	# echo $staleDate
 fi
 
-if [ ! "$URL" == "$staleURL" -a "$title" == "$staleTitle" -a "$date" == "$staleDate" -o ! -f $staleCache ]
+if [ ! "$URL" == "$staleURL" -a "$title" == "$staleTitle" -a "$date" == "$staleDate" -o ! -f $confDir"/url" -o ! -f $confDir"/title" -o ! -f $confDir"/date" ]
 then
 	echo "There is unread news on $archNews"
 	echo 
@@ -48,14 +48,19 @@ then
 	read -p "Do you wish to open the most recent news in your default browser? [y/N/a]" browse
 	case $browse in
 		[Yy]* ) xdg-open $URL;;
-		[Aa]* ) exit;;
+		[Aa]* ) rm $tempCache; exit;;
 	esac
 
-	cat $tempCache > $staleCache
-	rm $tempCache
+	
+
+	echo $URL > $confDir'/url'
+	echo $title > $confDir'/title'
+	echo $date > $confDir'/date'
 else
 	echo "There is no unread news on $archNews"
 fi
+
+rm $tempCache
 
 if [ -f "/usr/bin/pacman" ]
 then
