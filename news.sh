e915da65462036d1d59b84228896d92dc383f9cc
liefswanson <liefs@sfu.ca>
2014-10-16 22:58:45 -0700

    added a check to see if the news had already been read. Additionally refactored some of the cut statements to be more concise. Added a directory under /home/lief/.anh to store the cache which is being checked against.

--- news.sh
+++ news.sh
@@ -1,32 +1,57 @@
 #!/bin/bash
 
 archNews="https://archlinux.org/news/"
+#for testing so I don't DDOS archlinux.org by accident
 localNews="news.html"
 
+confDir=$HOME"/.anh/"
+tempCache=$confDir"tempCache.html"
+staleCache=$confDir"cache.html"
+
+#curl $archNews > $tempCache
+cat $localNews > $tempCache
+
+
 #grabs the relative link to the most recent news post then completes it into a complete url
-#firsturl=archNews"$(curl $archNews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
-postURL=$archNews"$(cat $localNews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
-localfirsturl="latest.html"
+URL=$archNews"$(cat $tempCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)"
 
-echo $postURL
+echo $URL
 
-postTitle="$(cat $localNews | grep title=\"View:.*\"\> | head -1 | cut -d\" -f 2 | cut -c 7-)"
+title="$(cat $tempCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
 
-echo $postTitle
+echo $title
 
 #grabs the date of the most recent news post
-#dateofurl="$(curl $archNews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
-dateOfPost="$(cat $localNews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
+date="$(cat $tempCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
 
-echo $dateOfPost
+echo $date
 
-read -p "Do you wish to open the most recent news in your default browser? [y/N/a]" yn
-case $yn in
-	[Yy]* ) xdg-open $firstURL;;
-	[Aa]* ) exit;;
-esac
+staleURL=$archNews"$(cat $staleCache | grep href=\"/news/ | head -1 | cut -d\" -f 4)" 
+staleTitle="$(cat $staleCache | grep 'title=\"View: ' | head -1 | cut -d\" -f 2 | cut -c 7-)"
+staleDate="$(cat $staleCache | grep [2-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] | head -1 | cut -c 21-30 )"
 
-if [ -f "/usr/bin/yaourt" ];
+echo $staleURL
+echo $staleTitle
+echo $staleDate
+
+if [ ! "$URL" == "$staleURL" -a "$title" == "$staleTitle" -a "$date" == "$staleDate" ]
 then
-	yaourt -Syu
+
+	
+	
+	read -p "Do you wish to open the most recent news in your default browser? [y/N/a]" browse
+	case $browse in
+		[Yy]* ) xdg-open $postURL;;
+		[Aa]* ) exit;;
+	esac
+
+	sleep 10
+
+	if [ -f "/usr/bin/pacman" ]
+	then
+		yaourt -Syu
+	fi
+else
+	echo "No new news."
 fi
+
