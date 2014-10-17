93b34515d69be19071490049627843a58654fbdf
liefswanson <liefs@sfu.ca>
2014-10-16 18:13:44 -0700

    added an abort option when prompting the user.

--- news.sh
+++ news.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-archnews="https://archlinux.org/news/"
-localnews="news.html"
+archNews="https://archlinux.org/news/"
+localNews="news.html"
 
 #grabs the relative link to the most recent news post then completes it into a complete url
-#firsturl=archnews"$(curl $archnews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
-firsturl=$archnews"$(cat $localnews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
+#firsturl=archNews"$(curl $archNews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
+postURL=$archNews"$(cat $localNews | grep href=\"/news/ | head -1 | cut -c 49- | rev | cut -c 2- | rev)"
 localfirsturl="latest.html"
 
-echo $firsturl
+echo $postURL
+
+postTitle="$(cat $localNews | grep title=\"View:.*\"\> | head -1 | cut -d\" -f 2 | cut -c 7-)"
+
+echo $postTitle
 
 #grabs the date of the most recent news post
-#dateofurl="$(curl $archnews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
-dateofurl="$(cat $localnews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
+#dateofurl="$(curl $archNews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
+dateOfPost="$(cat $localNews | grep [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] | head -1 | cut -c 21-30 )"
 
-echo $dateofurl
+echo $dateOfPost
 
-read -p "Do you wish to open the most recent news in your default browser? [y/N]" yn
+read -p "Do you wish to open the most recent news in your default browser? [y/N/a]" yn
 case $yn in
-	[Yy]* ) xdg-open $firsturl;
+	[Yy]* ) xdg-open $firstURL;;
+	[Aa]* ) exit;;
 esac
 
 if [ -f "/usr/bin/yaourt" ];
